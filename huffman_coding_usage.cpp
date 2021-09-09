63c3c1ccc1b7dd276d6bceacfd6195e2d2d9ce38
anupam96786 <anupam96786@gmail.com>
2021-09-09 22:42:45 +0530

    huffman coding usage file added

--- huffman_coding_usage.cpp
+++ huffman_coding_usage.cpp
@@ -10,10 +10,17 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    // creating two HuffmanCoding class
     HuffmanCoding hc1, hc2;
+
+    // encoding "appddd" and storing the encoded string into 'encodedString' variable
     string encodedString =  hc1.encode("appddd");
     cout << encodedString << endl;
+
+    // getting the decode table and storing it in 'dt' variable of DecodeTable datatype
     DecodeTable dt =  hc1.getDecodeTable();
+
+    // using the 'encodedString' and 'dt' (decode table) to decode the encoded string using another HuffmanCoding object
     cout << hc2.decode(encodedString, dt);
     return 0;
 }
